a68cd6e5f98e34da177e968293725e5b8b4ae4e3
MichaelWang <guanyi.wang@okcoin.net>
2020-04-22 17:46:28 +0800

    FINISH: slashing unjail ut

---
+++ module/slashing/tx_test.go
@@ -0,0 +1,52 @@
+package slashing
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/okex/okchain-go-sdk/mocks"
+	"github.com/okex/okchain-go-sdk/module/auth"
+	sdk "github.com/okex/okchain-go-sdk/types"
+	"github.com/okex/okchain-go-sdk/utils"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+const (
+	addr      = "okchain1dcsxvxgj374dv3wt9szflf9nz6342juzzkjnlz"
+	name      = "alice"
+	passWd    = "12345678"
+	accPubkey = "okchainpub1addwnpepqgzuks5c07kfce85e0t0x8qkuvvxu874965ruafn6svhjrhswt0lgdj85lv"
+	mnemonic  = "dumb thought reward exhibit quick manage force imitate blossom vendor ketchup sniff"
+	memo      = "my memo"
+)
+
+func TestSlashingClient_Unjail(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	config, err := sdk.NewClientConfig("testURL", "testChain", sdk.BroadcastBlock, "0.01okt", 200000)
+	require.NoError(t, err)
+	mockCli := mocks.NewMockClient(t, ctrl, config)
+	mockCli.RegisterModule(NewSlashingClient(mockCli.MockBaseClient), auth.NewAuthClient(mockCli.MockBaseClient))
+
+	fromInfo, _, err := utils.CreateAccountWithMnemo(mnemonic, name, passWd)
+	require.NoError(t, err)
+
+	accBytes := mockCli.BuildAccountBytes(addr, accPubkey, "1024okt", 1, 2)
+	expectedCdc := mockCli.GetCodec()
+	mockCli.EXPECT().GetCodec().Return(expectedCdc)
+	mockCli.EXPECT().Query(gomock.Any(), gomock.Any()).Return(accBytes, nil)
+
+	accInfo, err := mockCli.Auth().QueryAccount(addr)
+	require.NoError(t, err)
+
+	mockCli.EXPECT().BuildAndBroadcast(
+		fromInfo.GetName(), passWd, memo, gomock.AssignableToTypeOf([]sdk.Msg{}), accInfo.GetAccountNumber(), accInfo.GetSequence()).
+		Return(mocks.DefaultMockSuccessTxResponse(), nil)
+
+	res, err := mockCli.Slashing().Unjail(fromInfo, passWd, memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.NoError(t, err)
+	require.Equal(t, uint32(0), res.Code)
+
+	res, err = mockCli.Slashing().Unjail(fromInfo, "", memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.Error(t, err)
+
+}

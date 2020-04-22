632086efc8d278e691d87c8be87304295b9f337b
MichaelWang <guanyi.wang@okcoin.net>
2020-04-22 17:20:08 +0800

    FINISH: token multi-send ut

--- module/token/tx_test.go
+++ module/token/tx_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/okex/okchain-go-sdk/mocks"
 	"github.com/okex/okchain-go-sdk/module/auth"
+	"github.com/okex/okchain-go-sdk/module/token/types"
 	sdk "github.com/okex/okchain-go-sdk/types"
 	"github.com/okex/okchain-go-sdk/utils"
 	"github.com/stretchr/testify/require"
@@ -39,6 +40,45 @@ func TestTokenClient_Send(t *testing.T) {
 
 	res, err = mockCli.Token().Send(fromInfo, passWd, recAddr[1:], "10.24okt", memo, accInfo.GetAccountNumber(),
 		accInfo.GetSequence())
+	require.Error(t, err)
+
+}
+
+func TestTokenClient_MultiSend(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	config, err := sdk.NewClientConfig("testURL", "testChain", sdk.BroadcastBlock, "0.01okt", 200000)
+	require.NoError(t, err)
+	mockCli := mocks.NewMockClient(t, ctrl, config)
+	mockCli.RegisterModule(NewTokenClient(mockCli.MockBaseClient), auth.NewAuthClient(mockCli.MockBaseClient))
+
+	fromInfo, _, err := utils.CreateAccountWithMnemo(mnemonic, name, passWd)
+	require.NoError(t, err)
+
+	accBytes := mockCli.BuildAccountBytes(addr, accPubkey, "1024okt", 1, 2)
+	expectedCdc := mockCli.GetCodec()
+	mockCli.EXPECT().GetCodec().Return(expectedCdc)
+	mockCli.EXPECT().Query(gomock.Any(), gomock.Any()).Return(accBytes, nil)
+
+	accInfo, err := mockCli.Auth().QueryAccount(addr)
+	require.NoError(t, err)
+
+	mockCli.EXPECT().BuildAndBroadcast(
+		fromInfo.GetName(), passWd, memo, gomock.AssignableToTypeOf([]sdk.Msg{}), accInfo.GetAccountNumber(), accInfo.GetSequence()).
+		Return(mocks.DefaultMockSuccessTxResponse(), nil)
+
+	transfersStr := `okchain1wux20ku36ntgtxpgm7my9863xy3fqs0xgh66d7 1024okt,2048btc
+okchain1npm82ty95j9s7xja5s92hajwszdklh7kch23as 20.48okt`
+	transfers, err := utils.ParseTransfersStr(transfersStr)
+	require.NoError(t, err)
+	res, err := mockCli.Token().MultiSend(fromInfo, passWd, transfers, memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.NoError(t, err)
+	require.Equal(t, uint32(0), res.Code)
+
+	var emptyTransfer types.TransferUnit
+	badTransfers := []types.TransferUnit{emptyTransfer}
+
+	_, err = mockCli.Token().MultiSend(fromInfo, passWd, badTransfers, memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
 	require.Error(t, err)
 
 }

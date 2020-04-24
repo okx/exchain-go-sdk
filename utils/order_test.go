50cf35d4bd669d72bcae34779d66ecab8ad9d667
MichaelWang <guanyi.wang@okcoin.net>
2020-04-24 19:54:59 +0800

    OPTIMIZE: promote ut coverage for order&&errors tools

---
+++ utils/order_test.go
@@ -0,0 +1,81 @@
+package utils
+
+import (
+	"encoding/json"
+	order "github.com/okex/okchain-go-sdk/module/order/types"
+	sdk "github.com/okex/okchain-go-sdk/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestGetOrderIDsFromResponse(t *testing.T) {
+	mockOrderIDs := []string{"ID0000000000-1", "ID0000000000-2", "ID0000000000-3", "ID0000000000-4", "ID0000000000-5"}
+	var orderResults, fakeOrderResults1, fakeOrderResults2 []order.OrderResult
+	for i, orderID := range mockOrderIDs {
+		mockOrderRes := buildMockOrderRes(orderID)
+		if i < 3 {
+			orderResults = append(orderResults, mockOrderRes)
+		} else if i == 3 {
+			fakeOrderResults1 = append(fakeOrderResults1, mockOrderRes)
+		} else {
+			fakeOrderResults2 = append(fakeOrderResults2, mockOrderRes)
+		}
+	}
+
+	rawStrs := getRawStrSlice(orderResults, fakeOrderResults1, fakeOrderResults2)
+	require.Equal(t, 3, len(rawStrs))
+
+	// build mock TxResponse
+	mockTxResp := sdk.TxResponse{
+		Events: sdk.StringEvents{
+			{
+				Type: "message",
+				Attributes: []sdk.Attribute{
+					{
+						Key:   "not orders",
+						Value: rawStrs[1],
+					},
+					{
+						Key:   "orders",
+						Value: rawStrs[0], // target
+					},
+				},
+			},
+			{
+				Type: "not message",
+				Attributes: []sdk.Attribute{
+					{
+						Key:   "not orders",
+						Value: rawStrs[1],
+					},
+					{
+						Key:   "orders",
+						Value: rawStrs[2],
+					},
+				},
+			},
+		},
+	}
+
+	orderIDs := GetOrderIDsFromResponse(&mockTxResp)
+	require.Equal(t, mockOrderIDs[:3], orderIDs)
+}
+
+func getRawStrSlice(orderResults ...[]order.OrderResult) (strs []string) {
+	for _, res := range orderResults {
+		bytes, err := json.Marshal(res)
+		if err != nil {
+			panic(err)
+		}
+		strs = append(strs, string(bytes))
+	}
+
+	return
+}
+
+func buildMockOrderRes(orderID string) order.OrderResult {
+	return order.OrderResult{
+		Message: "default message",
+		OrderID: orderID,
+	}
+}

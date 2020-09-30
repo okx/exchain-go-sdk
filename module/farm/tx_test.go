1d4137fe0a804362141e3eeef06a53afcb5a02b1
MichaelWang <michael_wangguanyi@163.com>
2020-09-30 15:10:05 +0800

    FINISH: provide ut

--- module/farm/tx_test.go
+++ module/farm/tx_test.go
@@ -107,3 +107,56 @@ func TestFarmClient_DestroyPool(t *testing.T) {
 		accInfo.GetSequence())
 	require.Error(t, err)
 }
+
+func TestFarmClient_Provide(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	config, err := sdk.NewClientConfig("testURL", "testChain", sdk.BroadcastBlock, "", 200000,
+		1.1, "0.00000001okt")
+	require.NoError(t, err)
+	mockCli := mocks.NewMockClient(t, ctrl, config)
+	mockCli.RegisterModule(NewFarmClient(mockCli.MockBaseClient), auth.NewAuthClient(mockCli.MockBaseClient))
+
+	fromInfo, _, err := utils.CreateAccountWithMnemo(mnemonic, name, passWd)
+	require.NoError(t, err)
+
+	accBytes := mockCli.BuildAccountBytes(addr, accPubkey, "1024okt", 1, 2)
+	expectedCdc := mockCli.GetCodec()
+	mockCli.EXPECT().GetCodec().Return(expectedCdc)
+	mockCli.EXPECT().Query(gomock.Any(), gomock.Any()).Return(accBytes, nil)
+
+	accInfo, err := mockCli.Auth().QueryAccount(addr)
+	require.NoError(t, err)
+
+	mockCli.EXPECT().BuildAndBroadcast(
+		fromInfo.GetName(), passWd, memo, gomock.AssignableToTypeOf([]sdk.Msg{}), accInfo.GetAccountNumber(), accInfo.GetSequence()).
+		Return(mocks.DefaultMockSuccessTxResponse(), nil)
+
+	res, err := mockCli.Farm().Provide(fromInfo, passWd, "test-pool-name", "1024.1024okt",
+		"2048.2048", 1024, memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.NoError(t, err)
+	require.Equal(t, uint32(0), res.Code)
+
+	_, err = mockCli.Farm().Provide(fromInfo, passWd, "", "1024.1024okt",
+		"2048.2048", 1024, memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.Error(t, err)
+
+	_, err = mockCli.Farm().Provide(fromInfo, "", "test-pool-name", "1024.1024okt",
+		"2048.2048", 1024, memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.Error(t, err)
+
+	_, err = mockCli.Farm().Provide(fromInfo, passWd, "test-pool-name", "1024.1024",
+		"2048.2048", 1024, memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.Error(t, err)
+
+	_, err = mockCli.Farm().Provide(fromInfo, passWd, "test-pool-name", "1024.1024okt",
+		"", 1024, memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.Error(t, err)
+
+	mockCli.EXPECT().BuildAndBroadcast(
+		fromInfo.GetName(), passWd, memo, gomock.AssignableToTypeOf([]sdk.Msg{}), accInfo.GetAccountNumber(), accInfo.GetSequence()).
+		Return(sdk.TxResponse{}, errors.New("default error"))
+	_, err = mockCli.Farm().Provide(fromInfo, passWd, "test-pool-name", "1024.1024okt",
+		"2048.2048", 1024, memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.Error(t, err)
+}

7cf20b6b8befb8c21cbe1b6fd0727cb17cfa3aec
MichaelWang <michael_wangguanyi@163.com>
2020-12-03 14:46:58 +0800

    OPTIMIZE: increase the ut coverage of order

--- utils/order_test.go
+++ utils/order_test.go
@@ -9,6 +9,40 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	defaultProduct = "usdk_okt"
+	defaultSide    = "BUY"
+	defaultDecStr  = "1024.1024"
+)
+
+func TestBuildOrderItems(t *testing.T) {
+	expectedProductsStrs := []string{defaultProduct, defaultProduct}
+	expectedSidesStrs := []string{defaultSide, defaultSide}
+	expectedPricesStrs := []string{defaultDecStr, defaultDecStr}
+	expectedQuantitiesStrs := []string{defaultDecStr, defaultDecStr}
+	expectedDec := sdk.MustNewDecFromStr(defaultDecStr)
+
+	orderItems, err := BuildOrderItems(expectedProductsStrs, expectedSidesStrs, expectedPricesStrs, expectedQuantitiesStrs)
+	require.NoError(t, err)
+	require.Equal(t, 2, len(orderItems))
+	for _, item := range orderItems {
+		require.Equal(t, defaultProduct, item.Product)
+		require.Equal(t, defaultSide, item.Side)
+		require.Equal(t, expectedDec, item.Price)
+		require.Equal(t, expectedDec, item.Price)
+	}
+
+	badDecStr := "1.024.1024.1024"
+	expectedPricesStrs = []string{badDecStr, defaultDecStr}
+	_, err = BuildOrderItems(expectedProductsStrs, expectedSidesStrs, expectedPricesStrs, expectedQuantitiesStrs)
+	require.Error(t, err)
+
+	expectedPricesStrs = []string{defaultDecStr, defaultDecStr}
+	expectedQuantitiesStrs = []string{badDecStr, defaultDecStr}
+	_, err = BuildOrderItems(expectedProductsStrs, expectedSidesStrs, expectedPricesStrs, expectedQuantitiesStrs)
+	require.Error(t, err)
+}
+
 func TestGetOrderIDsFromResponse(t *testing.T) {
 	mockOrderIDs := []string{"ID0000000000-1", "ID0000000000-2", "ID0000000000-3", "ID0000000000-4", "ID0000000000-5"}
 	var orderResults, fakeOrderResults1, fakeOrderResults2 []ordertypes.OrderResult

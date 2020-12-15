20a1068e73211f2b104c3b2b834258bd7f289cf9
MichaelWang <michael_wangguanyi@163.com>
2020-12-15 16:03:22 +0800

    FINISH: create contract tx ut in evm module

--- module/evm/tx_test.go
+++ module/evm/tx_test.go
@@ -1,6 +1,7 @@
 package evm
 
 import (
+	"errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/golang/mock/gomock"
 	"github.com/okex/okexchain-go-sdk/mocks"
@@ -54,7 +55,57 @@ func TestEvmClient_SendTx(t *testing.T) {
 		accInfo.GetAccountNumber(), accInfo.GetSequence())
 	require.Error(t, err)
 
-	_, err = mockCli.Evm().SendTx(fromInfo, passWd, recAddr, "1000000000000000000", "0x0123456789abcdefg",
+	_, err = mockCli.Evm().SendTx(fromInfo, passWd, recAddr, "1000000000000000000", badPayloadStr, memo,
+		accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.Error(t, err)
+}
+
+func TestEvmClient_CreateContract(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	config, err := gosdktypes.NewClientConfig("testURL", "testChain", gosdktypes.BroadcastBlock, "",
+		200000, 1.1, "0.00000001okt")
+	require.NoError(t, err)
+	mockCli := mocks.NewMockClient(t, ctrl, config)
+	mockCli.RegisterModule(NewEvmClient(mockCli.MockBaseClient), auth.NewAuthClient(mockCli.MockBaseClient))
+
+	fromInfo, _, err := utils.CreateAccountWithMnemo(mnemonic, name, passWd)
+	require.NoError(t, err)
+
+	accBytes := mockCli.BuildAccountBytes(addr, accPubkey, "", "1024okt", 1, 2)
+	expectedCdc := mockCli.GetCodec()
+	mockCli.EXPECT().GetCodec().Return(expectedCdc).Times(2)
+	mockCli.EXPECT().Query(gomock.Any(), gomock.Any()).Return(accBytes, int64(1024), nil)
+
+	accInfo, err := mockCli.Auth().QueryAccount(addr)
+	require.NoError(t, err)
+
+	mockCli.EXPECT().GetConfig().Return(gosdktypes.ClientConfig{Gas: 200000}).Times(2)
+	mockCli.EXPECT().BuildAndBroadcast(
+		fromInfo.GetName(), passWd, memo, gomock.AssignableToTypeOf([]sdk.Msg{}), accInfo.GetAccountNumber(), accInfo.GetSequence()).
+		Return(mocks.DefaultMockSuccessTxResponse(), nil)
+
+	res, contractAddr, err := mockCli.Evm().CreateContract(fromInfo, passWd, "1000000000000000000", defaultPayloadStr,
+		memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.NoError(t, err)
+	require.Equal(t, uint32(0), res.Code)
+	require.NotZero(t, len(contractAddr))
+
+	_, contractAddr, err = mockCli.Evm().CreateContract(fromInfo, passWd, "1000000000000000000", badPayloadStr,
 		memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
 	require.Error(t, err)
+	require.Zero(t, len(contractAddr))
+
+	_, contractAddr, err = mockCli.Evm().CreateContract(fromInfo, passWd, "1000000000000000000okt", defaultPayloadStr,
+		memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.Error(t, err)
+	require.Zero(t, len(contractAddr))
+
+	mockCli.EXPECT().BuildAndBroadcast(
+		fromInfo.GetName(), passWd, memo, gomock.AssignableToTypeOf([]sdk.Msg{}), accInfo.GetAccountNumber(), accInfo.GetSequence()).
+		Return(sdk.TxResponse{}, errors.New("default error"))
+	_, contractAddr, err = mockCli.Evm().CreateContract(fromInfo, passWd, "1000000000000000000", defaultPayloadStr,
+		memo, accInfo.GetAccountNumber(), accInfo.GetSequence())
+	require.Error(t, err)
+	require.Zero(t, len(contractAddr))
 }

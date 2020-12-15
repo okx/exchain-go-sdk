e9b9def74ecb58d89469350fe72b038779d2b2c8
MichaelWang <46394603+RevelationOfTuring@users.noreply.github.com>
2020-12-15 16:04:44 +0800

    FINISH: ut of evm txs (#102)
    * FINISH: send tx ut in evm module
    
    * FINISH: create contract tx ut in evm module

---
+++ module/evm/query_test.go
@@ -0,0 +1,14 @@
+package evm
+
+const (
+	addr              = "okexchain1ntvyep3suq5z7789g7d5dejwzameu08m6gh7yl"
+	name              = "alice"
+	passWd            = "12345678"
+	accPubkey         = "okexchainpub17weu6qepq0ph2t3u697qar7rmdtdtqp4744jcprjd2h356zr0yh5vmw38a3my4vqjx5"
+	mnemonic          = "giggle sibling fun arrow elevator spoon blood grocery laugh tortoise culture tool"
+	memo              = "my memo"
+	recAddr           = "okexchain193xnjknz3e52mqv2nyufnzjugu3mh65rpxdasn"
+	recAddrEth        = "0x2c4d395A628e68aD818a9938998a5C4723BBEa83"
+	defaultPayloadStr = "0x0123456789abcdef"
+	badPayloadStr     = "0x0123456789abcdefg"
+)

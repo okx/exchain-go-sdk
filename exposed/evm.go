49a4052d7a44c4e5e6944bac4f2eeb137203e457
MichaelWang <michael_wangguanyi@163.com>
2020-12-17 11:02:45 +0800

    FINISH: query code in evm module (#109)

--- exposed/evm.go
+++ exposed/evm.go
@@ -3,6 +3,7 @@ package exposed
 import (
 	"github.com/cosmos/cosmos-sdk/crypto/keys"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/okex/okexchain-go-sdk/module/evm/types"
 	gosdktypes "github.com/okex/okexchain-go-sdk/types"
 )
 
@@ -10,6 +11,7 @@ import (
 type Evm interface {
 	gosdktypes.Module
 	EvmTx
+	EvmQuery
 }
 
 // EvmTx shows the expected tx behavior for inner evm client
@@ -18,4 +20,9 @@ type EvmTx interface {
 		sdk.TxResponse, error)
 	CreateContract(fromInfo keys.Info, passWd, amountStr, payloadStr, memo string, accNum, seqNum uint64) (
 		sdk.TxResponse, string, error)
+}
+
+// EvmQuery shows the expected query behavior for inner evm client
+type EvmQuery interface {
+	QueryCode(contractAddrStr string) (types.QueryResCode, error)
 }
--- module/evm/query.go
+++ module/evm/query.go
@@ -1,3 +1,34 @@
 package evm
 
-//TODO
+import (
+	"errors"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/okex/okexchain-go-sdk/module/evm/types"
+	"github.com/okex/okexchain-go-sdk/utils"
+	evmtypes "github.com/okex/okexchain/x/evm/types"
+	"strings"
+)
+
+// QueryCode gets the contract code from OKExChain
+func (ec evmClient) QueryCode(contractAddrStr string) (resCode types.QueryResCode, err error) {
+	if !strings.HasPrefix(contractAddrStr, "0x") {
+		contractAddrStr = fmt.Sprintf("0x%s", contractAddrStr)
+	}
+
+	if !common.IsHexAddress(contractAddrStr) {
+		return resCode, errors.New("failed. invalid contract address")
+	}
+
+	path := fmt.Sprintf("custom/%s/code/%s", evmtypes.RouterKey, common.HexToAddress(contractAddrStr).Hex())
+	res, _, err := ec.Query(path, nil)
+	if err != nil {
+		return resCode, utils.ErrClientQuery(err.Error())
+	}
+
+	if err = ec.GetCodec().UnmarshalJSON(res, &resCode); err != nil {
+		return resCode, utils.ErrUnmarshalJSON(err.Error())
+	}
+
+	return
+}

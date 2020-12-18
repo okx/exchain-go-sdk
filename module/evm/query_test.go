f231d6afcf00851d28dbf802e6b3ac98a5d982ac
MichaelWang <michael_wangguanyi@163.com>
2020-12-18 11:40:39 +0800

    FINISH: query storage at xxx ut in evm (#114)

--- module/evm/query_test.go
+++ module/evm/query_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/okex/okexchain-go-sdk/mocks"
 	gosdktypes "github.com/okex/okexchain-go-sdk/types"
+	"github.com/okex/okexchain-go-sdk/utils"
 	evmtypes "github.com/okex/okexchain/x/evm/types"
 	"github.com/stretchr/testify/require"
 	"strings"
@@ -27,6 +28,8 @@ const (
 
 	contractAddr = "0x9aD84c8630E0282F78e5479B46E64E17779e3Cfb"
 	codeContent  = "default code content"
+	storageValue = "default storage value"
+	keyStr       = "defaultKey"
 )
 
 func TestEvmClient_QueryCode(t *testing.T) {
@@ -62,5 +65,39 @@ func TestEvmClient_QueryCode(t *testing.T) {
 
 	mockCli.EXPECT().Query(expectedPath, nil).Return(expectedRet[1:], int64(1024), nil)
 	_, err = mockCli.Evm().QueryCode(contractAddr)
+	require.Error(t, err)
+}
+
+func TestEvmClient_QueryStorageAt(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	config, err := gosdktypes.NewClientConfig("testURL", "testChain", gosdktypes.BroadcastBlock, "",
+		200000, 1.1, "0.00000001okt")
+	require.NoError(t, err)
+	mockCli := mocks.NewMockClient(t, ctrl, config)
+	mockCli.RegisterModule(NewEvmClient(mockCli.MockBaseClient))
+
+	expectedRet := mockCli.BuildQueryResStorage(storageValue)
+	expectedCdc := mockCli.GetCodec()
+	expectedPath := fmt.Sprintf("custom/%s/storage/%s/%s", evmtypes.RouterKey, common.HexToAddress(contractAddr).Hex(),
+		utils.FormatKeyToHash(keyStr))
+
+	mockCli.EXPECT().GetCodec().Return(expectedCdc).Times(3)
+	mockCli.EXPECT().Query(expectedPath, nil).Return(expectedRet, int64(1024), nil).Times(2)
+
+	resStorage, err := mockCli.Evm().QueryStorageAt(contractAddr, keyStr)
+	require.NoError(t, err)
+	require.True(t, strings.EqualFold(storageValue, string(resStorage.Value)))
+
+	resStorage, err = mockCli.Evm().QueryStorageAt(contractAddr[2:], keyStr)
+	require.NoError(t, err)
+	require.True(t, strings.EqualFold(storageValue, string(resStorage.Value)))
+
+	mockCli.EXPECT().Query(expectedPath, nil).Return(nil, int64(0), errors.New("default error"))
+	_, err = mockCli.Evm().QueryStorageAt(contractAddr, keyStr)
+	require.Error(t, err)
+
+	mockCli.EXPECT().Query(expectedPath, nil).Return(expectedRet[1:], int64(1024), nil)
+	_, err = mockCli.Evm().QueryStorageAt(contractAddr, keyStr)
 	require.Error(t, err)
 }

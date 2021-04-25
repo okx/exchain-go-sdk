39a3ba20bacb0b6f629dce2b75228c5f9eac4823
MichaelWang <michael_wangguanyi@163.com>
2021-04-25 19:31:08 +0800

    FINISH: add val prefix converting

--- utils/address.go
+++ utils/address.go
@@ -1,18 +1,36 @@
 package utils
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
 
 // AccAddrPrefixConvert converts the account address between two different prefixes
 func AccAddrPrefixConvert(srcPrefx, srcAccAddrStr, dstPrefix string) (dstAccAddrStr string, err error) {
 	config := sdk.GetConfig()
 	// set source prefix
-	config.SetBech32PrefixForAccount(srcPrefx, srcPrefx+"pub")
+	config.SetBech32PrefixForAccount(srcPrefx, fmt.Sprintf("%s%s", srcPrefx, sdk.PrefixPublic))
 	accAddr, err := sdk.AccAddressFromBech32(srcAccAddrStr)
 	if err != nil {
 		return
 	}
 
 	// set destination prefix
-	config.SetBech32PrefixForAccount(dstPrefix, dstPrefix+"pub")
+	config.SetBech32PrefixForAccount(dstPrefix, fmt.Sprintf("%s%s", dstPrefix, sdk.PrefixPublic))
 	return accAddr.String(), err
+}
+
+// ValAddrPrefixConvert converts the validator address between two different prefixes
+func ValAddrPrefixConvert(srcPrefx, srcValAddrStr, dstPrefix string) (dstValAddrStr string, err error) {
+	config := sdk.GetConfig()
+	// set source prefix
+	config.SetBech32PrefixForValidator(srcPrefx, fmt.Sprintf("%s%s", srcPrefx, sdk.PrefixPublic))
+	valAddr, err := sdk.ValAddressFromBech32(srcValAddrStr)
+	if err != nil {
+		return
+	}
+
+	// set destination prefix
+	config.SetBech32PrefixForValidator(dstPrefix, fmt.Sprintf("%s%s", dstPrefix, sdk.PrefixPublic))
+	return valAddr.String(), err
 }

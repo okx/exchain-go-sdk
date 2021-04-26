960b28eb2801849cbff63c0299c1d724a36f99a4
MichaelWang <michael_wangguanyi@163.com>
2021-04-26 18:54:47 +0800

    Merge PR: add validator address prefix converting function  && tag v0.18.1 (#134)
    * FINISH: add val prefix converting
    
    * FINISH: ut of val addr prefix converting
    
    * remove useless code
    
    * FIX: ut bug

--- utils/address_test.go
+++ utils/address_test.go
@@ -3,11 +3,14 @@ package utils
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
 const (
+	defaultValAddr             = "exvaloper1qj5c07sm6jetjz8f509qtrxgh4psxkv3m2wy6x"
 	accAddrWithOKExChainPrefix = "okexchain1qj5c07sm6jetjz8f509qtrxgh4psxkv32x0qas"
+	valAddrWithOKExChainPrefix = "okexchainvaloper1qj5c07sm6jetjz8f509qtrxgh4psxkv3tzllpj"
 )
 
 func TestAccAddrPrefixConvert(t *testing.T) {
@@ -28,4 +31,30 @@ func TestAccAddrPrefixConvert(t *testing.T) {
 	dstAccAddrStr, err = AccAddrPrefixConvert("ex", defaultAddr, "okexchainx")
 	require.NoError(t, err)
 	require.NotEqual(t, accAddrWithOKExChainPrefix, dstAccAddrStr)
+
+	// recover the account prefix
+	sdk.GetConfig().SetBech32PrefixForAccount("ex", "expub")
+}
+
+func TestValAddrPrefixConvert(t *testing.T) {
+	dstValAddrStr, err := ValAddrPrefixConvert("exvaloper", defaultValAddr, "okexchainvaloper")
+	require.NoError(t, err)
+	require.Equal(t, valAddrWithOKExChainPrefix, dstValAddrStr)
+
+	// error check
+	// wrong source prefix
+	_, err = ValAddrPrefixConvert("exxvaloper", defaultValAddr, "okexchainvaloper")
+	require.Error(t, err)
+
+	// wrong source validator address
+	_, err = ValAddrPrefixConvert("exvaloper", defaultValAddr+"a", "okexchainvaloper")
+	require.Error(t, err)
+
+	// wrong destination validator address
+	dstValAddrStr, err = ValAddrPrefixConvert("exvaloper", defaultValAddr, "okexchainxvaloper")
+	require.NoError(t, err)
+	require.NotEqual(t, valAddrWithOKExChainPrefix, dstValAddrStr)
+
+	// recover the validator prefix
+	sdk.GetConfig().SetBech32PrefixForValidator("exvaloper", "exvaloperpub")
 }

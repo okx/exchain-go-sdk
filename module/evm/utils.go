456aba7aa3b4fb330365266cf554029543711b55
MichaelWang <michael_wangguanyi@163.com>
2021-07-02 14:55:41 +0800

    optimize code

--- module/evm/utils.go
+++ module/evm/utils.go
@@ -4,21 +4,25 @@ import (
 	authcli "github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	ethcmn "github.com/ethereum/go-ethereum/common"
 	ethcore "github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/rlp"
 	evmtypes "github.com/okex/exchain/x/evm/types"
 	"github.com/tendermint/tendermint/crypto/tmhash"
 )
 
 // GetTxHash calculates the tx hash
 func (ec evmClient) GetTxHash(signedTx *ethcore.Transaction) (txHash ethcmn.Hash, err error) {
-	ethTxBytes, err := rlp.EncodeToBytes(signedTx)
-	if err != nil {
-		return
-	}
-
-	var tx evmtypes.MsgEthereumTx
-	if err = rlp.DecodeBytes(ethTxBytes, &tx); err != nil {
-		return
+	v, r, s := signedTx.RawSignatureValues()
+	tx := evmtypes.MsgEthereumTx{
+		Data: evmtypes.TxData{
+			AccountNonce: signedTx.Nonce(),
+			Price:        signedTx.GasPrice(),
+			GasLimit:     signedTx.Gas(),
+			Recipient:    signedTx.To(),
+			Amount:       signedTx.Value(),
+			Payload:      signedTx.Data(),
+			V:            v,
+			R:            r,
+			S:            s,
+		},
 	}
 
 	txBytes, err := authcli.GetTxEncoder(ec.GetCodec())(tx)

1bfa2f391934be69f6c194b96bd3c8677405ff8e
fan ke <fan.ke@okg.com>
2022-10-26 17:52:25 +0800

    add feesplit tx

--- exposed/feesplit.go
+++ exposed/feesplit.go
@@ -2,22 +2,40 @@ package exposed
 
 import (
 	gosdktypes "github.com/okex/exchain-go-sdk/types"
+	"github.com/okex/exchain/libs/cosmos-sdk/crypto/keys"
+	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	"github.com/okex/exchain/libs/cosmos-sdk/types/query"
 	"github.com/okex/exchain/x/feesplit/types"
 )
 
-// Token shows the expected behavior for inner token client
+// Feesplit shows the expected behavior for inner Feesplit client
 type Feesplit interface {
 	gosdktypes.Module
 	FeesplitTx
 	FeesplitQuery
 }
 
-// TokenTx shows the expected tx behavior for inner token client
+// FeesplitTx shows the expected tx behavior for inner Feesplit client
 type FeesplitTx interface {
+	// RegisterFeeSplit register a contract for fee distribution.
+	// **NOTE** Please ensure, that the deployer of the contract (or the factory that deployes the contract)
+	// is an account that is owned by your project
+	// to avoid that an individual deployer who leaves your project becomes malicious.
+	RegisterFeeSplit(fromInfo keys.Info, passWd string, accNum, seqNum uint64, memo string, contractAddress string, nonces []uint64, withdrawAddress string) (*sdk.TxResponse, error)
+
+	// CancelFeeSplit cancel a contract from fee distribution.
+	// The deployer will no longer receive fees from users interacting with the contract.
+	// Only the contract deployer can cancel a contract.
+	CancelFeeSplit(fromInfo keys.Info, passWd string, accNum, seqNum uint64, memo string, contractAddress string) (*sdk.TxResponse, error)
+
+	// UpdateFeeSplit update withdraw address for a contract registered for fee distribution.
+	// Only the contract deployer can update the withdraw address.
+	UpdateFeeSplit(fromInfo keys.Info, passWd string, accNum, seqNum uint64, memo string, contractAddress string, withdrawAddress string) (*sdk.TxResponse, error)
+
+	FeeSplitSharesProposal(fromInfo keys.Info, passWd string, accNum, seqNum uint64, memo string)
 }
 
-// FeesplitQuery shows the expected query behavior for inner token client
+// FeesplitQuery shows the expected query behavior for inner Feesplit client
 type FeesplitQuery interface {
 
 	// QueryFeesplits query all fee splits
